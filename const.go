4b8b41e42f50c0371226e8145a95877ce4f47cbf
Cixtor <cixtords@gmail.com>
2019-09-11 09:00:00 -0700

    Move the constants representing the threat levels to const.go

---
+++ const.go
@@ -0,0 +1,19 @@
+package main
+
+// ttUnspecified represents an unknown threat type.
+const ttUnspecified string = "UNSPECIFIED"
+
+// ttMalware represents a malware threat type.
+const ttMalware string = "MALWARE"
+
+// ttSocialEngineering represents a social engineering threat type.
+const ttSocialEngineering string = "SOCIAL_ENGINEERING"
+
+// ttUnwantedSoftware represents an unwanted software threat type.
+const ttUnwantedSoftware string = "UNWANTED_SOFTWARE"
+
+// ttPotentiallyHarmful represents a potentially harmful application threat type.
+const ttPotentiallyHarmful string = "POTENTIALLY_HARMFUL"
+
+// ttNone represents no malware infection.
+const ttNone string = "NONE"
--- threat_type.go
+++ threat_type.go
@@ -30,24 +30,6 @@ type ThreatInfo struct {
 	Cache    time.Duration `json:"cache,omitempty"`
 }
 
-// ttUnspecified represents an unknown threat type.
-const ttUnspecified string = "UNSPECIFIED"
-
-// ttMalware represents a malware threat type.
-const ttMalware string = "MALWARE"
-
-// ttSocialEngineering represents a social engineering threat type.
-const ttSocialEngineering string = "SOCIAL_ENGINEERING"
-
-// ttUnwantedSoftware represents an unwanted software threat type.
-const ttUnwantedSoftware string = "UNWANTED_SOFTWARE"
-
-// ttPotentiallyHarmful represents a potentially harmful application threat type.
-const ttPotentiallyHarmful string = "POTENTIALLY_HARMFUL"
-
-// ttNone represents no malware infection.
-const ttNone string = "NONE"
-
 // ThreatType inspects the hostname, port number, URL path and query string to
 // check if the request will possibly return a malicious page or not. A threat
 // level 0x0 means the URL has not been reported to contain malware prior the
